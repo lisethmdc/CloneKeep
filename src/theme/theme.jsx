e7547a3f460d26478512d2930eb44aa23f99b517
lisethmdc <lismc1992@gmail.com>
2022-04-09 06:21:59 +0200

    adding theme

---
+++ src/theme/theme.jsx
@@ -0,0 +1,14 @@
+const theme = {
+    palette: {
+        primary: '#f5ba13',
+        dark: '#353535',
+        light: '#f5f6f7',
+    },
+    scale: {
+        h1: '2rem',
+        h2: '1.5rem'
+    },
+    
+}
+
+export default theme;
